09129ce568b1ac53d3a39f6693c21a89843bd477
Rensy93 <rasmusflomen@live.se>
2016-06-10 00:19:20 +0200

    Add files via upload

---
+++ Test/Barista3000Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Barista3000;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Barista3000Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var coffeeMaker = new CoffeeMaker();
+
+            coffeeMaker.MakeDrink(new SparklingWater());
+
+            Assert.IsTrue(true, );
+            //Assert.Equals("", );
+        }
+    }
+}
